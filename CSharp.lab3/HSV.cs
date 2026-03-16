22eb46b42732abc6c608ee82c32ee9cde3332fa8
Utka <vlad-307123@mail.ru>
2026-03-16 21:27:52 +0800

    Был реализован конструктор и методы в классе HSV

--- CSharp.lab3/HSV.cs
+++ CSharp.lab3/HSV.cs
@@ -4,7 +4,58 @@ using System.Text;
 
 namespace CSharp.lab3
 {
-    internal class HSV
+    public class HSV
     {
+        private Color currentColor;
+        private TrackBar tbBlue;
+        private TrackBar tbRed;
+        private TrackBar tbGreen;
+        private TrackBar tbHSV;
+        private PictureBox displayPictureBox;
+
+        public HSV(TrackBar HSV, PictureBox pictureBox)
+        {
+            //     this.tbBlue = blue;
+            //    this.tbRed = red;
+            //    this.tbGreen = green;
+            this.tbHSV = HSV;
+            this.displayPictureBox = pictureBox;
+
+            InitializeTrackBars();
+        }
+
+        private void InitializeTrackBars()
+        {
+            tbHSV.Minimum = 0;
+            tbHSV.Maximum = 360;
+
+            tbBlue.Minimum = 0;
+            tbBlue.Maximum = 255;
+
+            tbRed.Minimum = 0;
+            tbRed.Maximum = 255;
+
+            tbGreen.Minimum = 0;
+            tbGreen.Maximum = 255;
+        }
+
+        public void UpdateColor(Color baseColor, int hueValue)
+        {
+            currentColor = baseColor;
+
+            // UpdatePictureBoxColor();
+        }
+
+        private void UpdatePictureBoxColor()
+        {
+            if (displayPictureBox != null)
+            {
+                displayPictureBox.BackColor = Color.FromArgb(
+                    tbRed.Value % 255,
+                    tbGreen.Value % 255,
+                    tbBlue.Value % 255
+                );
+            }
+        }
     }
 }
